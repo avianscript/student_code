6356f7231c0202b7c524319a33df0adf9f1f85ca
Yoav Morahg <yoav@techelevator.com>
2022-05-19 07:44:43 -0400

    m1d4 lecture

---
+++ module-1/04_Loops_and_Arrays/lecture/src/main/java/com/techelevator/Scratchpad.java
@@ -0,0 +1,20 @@
+package com.techelevator;
+
+public class Scratchpad {
+
+
+    public void examples() {
+
+    }
+
+    
+
+
+    // don't worry about what this is doing yet
+
+    public static void main(String[] args) {
+
+        Scratchpad scratchpad = new Scratchpad();
+        scratchpad.examples();
+    }
+}
