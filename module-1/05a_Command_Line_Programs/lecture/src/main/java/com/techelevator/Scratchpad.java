944fae3553e9091cd969fda16df55c42dabe177b
Yoav Morahg <yoav@techelevator.com>
2022-05-20 07:36:34 -0400

    m1d5 lecture

---
+++ module-1/05a_Command_Line_Programs/lecture/src/main/java/com/techelevator/Scratchpad.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+public class Scratchpad {
+
+    public static void main(String[] args) {
+        Scratchpad pad = new Scratchpad();
+        pad.run();
+    }
+
+    public void run() {
+
+    }
+}
