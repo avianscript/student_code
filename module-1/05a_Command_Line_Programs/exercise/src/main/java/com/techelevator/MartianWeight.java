83608d866927fe097e4763a426ea610a312bb297
Kevin Ozmon <kozmon1@gmail.com>
2022-05-21 19:55:43 -0400

    commit 1 for Sat

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/MartianWeight.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/MartianWeight.java
@@ -16,10 +16,34 @@ Enter a series of Earth weights (space-separated): 98 235 185
  235 lbs. on Earth is 88 lbs. on Mars.
  185 lbs. on Earth is 69 lbs. on Mars. 
  */
+import java.util.Scanner;
+
 public class MartianWeight {
+
 
 	public static void main(String[] args) {
 
-	}
+		Scanner earthlingWeight = new Scanner(System.in);
 
+		System.out.println("I am a Martian!");
+
+		System.out.println("Please input a list of earthling weights for me to convert into martian weight.");
+		System.out.println("Separate each weight with a single space. ");
+		String earthWeight = earthlingWeight.nextLine();
+
+		String[] listOfWeights = earthWeight.split(" ");
+
+
+		for (int i = 0; i < listOfWeights.length; i++) {
+			int numWeights = Integer.parseInt(listOfWeights[i]);
+			double convertedWeights = numWeights * 0.378;
+			int intConvertedWeights = (int)convertedWeights;
+			System.out.println(listOfWeights[i] + " lbs. on Earth is " + intConvertedWeights + " lbs. on Mars.");
+		}
+	}
 }
+
+
+
+
+
