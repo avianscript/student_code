8759d1645fe9a58becf682760b816cd2cbe5ea50
Kevin Ozmon <kozmon1@gmail.com>
2022-05-27 21:47:59 -0400

    checkpoint 1 commit for day 10

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Employee.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Employee.java
@@ -1,5 +1,49 @@
 package com.techelevator;
 
 public class Employee {
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String department;
+    private double annualSalary;
+
+    public Employee (int employeeId, String firstName, String lastName, double salary) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.annualSalary = salary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getFullName() {
+        return lastName + ", " + firstName;
+    }
+    public String getDepartment(){
+        return department;
+    }
+
+    public double getAnnualSalary(){
+        return annualSalary;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setDepartment(String lastName) {
+        this.department = department;
+    }
+
+    public void raiseSalary(double percent) {
+        annualSalary = (annualSalary * (percent / 100)) + annualSalary;
+    }
 
 }
--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -2,4 +2,81 @@ package com.techelevator;
 
 public class HomeworkAssignment {
 
-}
+    private int earnedMarks;
+    private int possibleMarks;
+    private String submitterName;
+    private String letterGrade;
+
+    public HomeworkAssignment(int possibleMarks, String submitterName) {
+        this.possibleMarks = possibleMarks;
+        this.submitterName = submitterName;
+    }
+
+    public int getEarnedMarks (){
+        return this.earnedMarks;
+    }
+    public int getPossibleMarks () {
+        return this.possibleMarks;
+    }
+    public String getSubmitterName () {
+        return this.submitterName;
+    }
+    public void setEarnedMarks (int earnedMarks) {
+        this.earnedMarks = earnedMarks;
+    }
+
+    public String getLetterGrade () {
+        if ((earnedMarks / (double)possibleMarks * 100) >= 90) {
+            letterGrade = "A";
+        } else if (earnedMarks / (double)possibleMarks * 100 > 79 && earnedMarks / possibleMarks * 100 < 90) {
+            letterGrade = "B";
+        } else if (earnedMarks / (double)possibleMarks * 100 > 69 && earnedMarks / possibleMarks * 100 < 80) {
+            letterGrade = "C";
+        } else if (earnedMarks / (double)possibleMarks * 100 > 59 && earnedMarks / possibleMarks * 100 < 70) {
+            letterGrade = "D";
+        } else {
+            letterGrade = "F";
+        }
+        return letterGrade;
+    }
+
+    }
+
+
+//    public static void main(String[] args) {
+//        HomeworkAssignment homeworkAssignment = new HomeworkAssignment();
+
+//        if (earnedMarks / possibleMarks * 100 >= 90) {
+//            letterGrade = "A";
+//        } else if (earnedMarks / possibleMarks * 100 > 79 && earnedMarks / possibleMarks * 100 < 90) {
+//            letterGrade = "B";
+//        } else if (earnedMarks / possibleMarks * 100 > 69 && earnedMarks / possibleMarks * 100 < 80) {
+//            letterGrade = "C";
+//        } else if (earnedMarks / possibleMarks * 100 > 59 && earnedMarks / possibleMarks * 100 < 70) {
+//            letterGrade = "D";
+//        } else {
+//            letterGrade = "F";
+//        }
+//        return "Your letter grade is: " + letterGrade;
+//    }
+//    }
+//}
+//    public HomeworkAssignment(int possibleMarks, String submitterName) {
+//    }
+//    public String HomeworkAssignment(int possibleMarks, int earnedMarks,String letterGrade, String submitterName) {
+//
+//        this.earnedMarks = earnedMarks;
+//        this.possibleMarks = possibleMarks;
+//        this.submitterName = submitterName;
+//        this.letterGrade = letterGrade;
+//        return letterGrade;
+//
+
+
+    //    HomeworkAssignment homeworkGrades = new HomeworkAssignment();
+
+//   homeworkGrades.setEarnedMarks(0);
+//    homeworkGrades.getEarnedMarks();
+//    homeworkGrades.getPossibleMarks();
+//    homeworkGrades.getSubmitterName();
+//    homeworkGrades.getLetterGrade();
