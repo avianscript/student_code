31ef2902cb5b1f67c3dd660d420c87bb3be361c3
Yoav Morahg <yoav@techelevator.com>
2022-06-21 12:06:13 -0400

    m2d7 lecture-final-day-1

---
+++ module-2/07_Data_Access_and_DAO/lecture-final-day-1/src/main/java/com/techelevator/SpringJdbcExamplesRunner.java
@@ -0,0 +1,23 @@
+package com.techelevator;
+
+public class SpringJdbcExamplesRunner {
+
+    public static void main(String[] args) {
+
+        SpringJdbcExamples examples = new SpringJdbcExamples();
+
+
+        // examples.listMovies();
+
+        //examples.printTitleById( 750);
+
+        //examples.printMovieCount();
+
+        //examples.printMovieTitle(11);
+
+        examples.printMoviesStartingWith("the");
+
+    }
+
+
+}
