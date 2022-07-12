5f18d72dcf500cc24ce3d8e2ee1ff1cb127cd93b
Kevin Ozmon <kozmon1@gmail.com>
2022-07-12 16:37:10 -0400

    commit for 7 12 22

--- Ecommerce Connect Beta/src/main/java/EcommerceMachine/Menus.java
+++ Ecommerce Connect Beta/src/main/java/EcommerceMachine/Menus.java
@@ -1,39 +1,99 @@
 package EcommerceMachine;
 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
 public class Menus {
 
-private static final String mainMenuProdInfo = "(1) View product information";
-private static final String mainMenuPrintOut = "(2) Print recommendations";
-private static final String mainMenuAddProd = "(3) Add product";
-private static final String mainMenuExit = "(4) Exit program";
-private static final String viewProdMenuList = "(1) List all products";
-private static final String viewProdMenuSearch = "(2) Search products by keyword";
-private static final String returnToMainMenu = "(R)eturn to main menu";
-private static final String recommendationsMenuRecommendations = "(1) Assign profit margin and see recommendations";
-private static final String addProdMenuInput = "(1) Input new product details";
+    Processor processor = new Processor();
+    Scanner userInput = new Scanner(System.in);
+
+    private static final String mainMenuProdInfo = "(1) View product information";
+    private static final String mainMenuPrintOut = "(2) Print recommendations";
+    private static final String mainMenuAddProd = "(3) Add product";
+    private static final String mainMenuExit = "(4) Exit program";
+    private static final String viewProdMenuList = "(1) List all products";
+    private static final String viewProdMenuSearch = "(2) Search products by keyword";
+    private static final String returnToMainMenu = "(R)eturn to main menu";
+    private static final String recommendationsMenuRecommendations = "(1) Assign profit margin and see recommendations";
+    private static final String addProdMenuInput = "(1) Input new product details";
+
+    public Menus() throws FileNotFoundException {
+    }
+
+    public void mainMenu() {
+        System.out.println(mainMenuProdInfo);
+        System.out.println(mainMenuPrintOut);
+        System.out.println(mainMenuAddProd);
+        System.out.println(mainMenuExit);
+        System.out.print("Please select an option: ");
+    }
+
+    public void prodMenu() {
+        System.out.println(viewProdMenuList);
+        System.out.println(viewProdMenuSearch);
+        System.out.println(returnToMainMenu);
+        System.out.print("Please select an option: ");
+    }
+
+    public void mainMenuRun() {
+        String menuInput = "";
+        while (!menuInput.equals("4")) {
+            mainMenu();
+            menuInput = userInput.nextLine();
+            if (menuInput.equals("1")) {
+                viewProdMenu();
+                continue;
+            } else if (menuInput.equals("2")) {
+                recommendationsMenu();
+                continue;
+            } else if (menuInput.equals("3")) {
+                addProdMenu();
+                continue;
+            }
+        }
+        System.out.println("Thanks. Goodbye.");
+    }
+
+    public void viewProdMenu() {
+        String prodMenuInput = "";
+        while (!prodMenuInput.equals("R")) {
+            prodMenu();
+            prodMenuInput = userInput.nextLine();
+            if (prodMenuInput.equals("1")) {
+                processor.printProducts();
+                continue;
+            } else if (prodMenuInput.equals("2")) {
+                System.out.println("Please input a search term: ");
+                String searchTerm = userInput.nextLine();
+                processor.keywordSearch(searchTerm);
+                continue;
+            }
+            }
+        }
 
 
-public void mainMenu () {
-    System.out.println(mainMenuProdInfo);
-    System.out.println(mainMenuPrintOut);
-    System.out.println(mainMenuAddProd);
-    System.out.println(mainMenuExit);
+    public void recommendationsMenu() {
+        System.out.println(recommendationsMenuRecommendations);
+        System.out.println(returnToMainMenu);
+        System.out.print("Please select an option: ");
+        String recMenuInput = userInput.nextLine();
+            if (recMenuInput.equals("1")) {
+                processor.printRecs();
+            }
+    }
+
+    public void addProdMenu() {
+        System.out.println(addProdMenuInput);
+        System.out.println(returnToMainMenu);
+        System.out.print("Select an option: ");
+        String addProdMenuSelection = userInput.nextLine();
+            if (addProdMenuSelection.equals("1")) {
+                processor.newInputProduct();
+            }
+        }
 }
 
-public void viewProdMenu () {
-    System.out.println(viewProdMenuList);
-    System.out.println(viewProdMenuSearch);
-    System.out.println(returnToMainMenu);
-}
 
-public void recommendationsMenu () {
-    System.out.println(recommendationsMenuRecommendations);
-    System.out.println(returnToMainMenu);
-}
-
-public void addProdMenu () {
-    System.out.println(addProdMenuInput);
-    System.out.println(returnToMainMenu);
-}
-
-}
--- Ecommerce Connect Beta/src/main/java/EcommerceMachine/Processor.java
+++ Ecommerce Connect Beta/src/main/java/EcommerceMachine/Processor.java
@@ -1,50 +1,46 @@
 package EcommerceMachine;
 
 import java.io.*;
+import java.time.LocalDate;
 import java.util.*;
 
 public class Processor {
-    List<String[]> listOfProducts = new ArrayList<>();
 
+    public final String listAffirmative = "LIST THIS ITEM! Desired margin places list price well below retail price.";
+    public final String listWithCaution = "LIST WITH CAUTION! Desired margin places list price within 5% of retail price.";
+    public final String listFalse = "DO NOT LIST! Desired margin places list price well above retail price.";
+    public final String listRange = "A list price within 5% of retail price would be ";
+    public final String listRangeProfit = "Listing within 5% of retail price would yield a profit margin range of ";
+
+    List<String[]> listOfProducts = new ArrayList<>();
+    List<String> recommendations = new ArrayList<>();
 
     //Scanner for use throughout class
     File csvFilePath = new File("custom_file.csv");
-    Scanner input = new Scanner(csvFilePath);
+    Scanner fileInput = new Scanner(csvFilePath);
+    Scanner userInput = new Scanner(System.in);
 
     public Processor() throws FileNotFoundException {
     }
 
     //Place to keep methods
 
-    //extract data from csv for display method
-    public void printProductList() {
-        while (input.hasNextLine()) {
-            String nextLine = input.nextLine();
-            System.out.println(nextLine);
-        }
-    }
-
     //extract data from csv and place into a list method
     public List<String[]> csvToList() {
-        while (input.hasNextLine()) {
-            String[] inputArray = input.nextLine().split(",");
+        while (fileInput.hasNextLine()) {
+            String[] inputArray = fileInput.nextLine().split(",");
             listOfProducts.add(inputArray);
         }
         return listOfProducts;
     }
 
-    //extract data from inventory list and place into ProductPojo
-    public void listToProduct() {
-        ProductPojo productPojo = new ProductPojo();
-        for (String [] product : csvToList()) {
-            productPojo.setProductUpc(product[0]);
-            productPojo.setProductName(product[1]);
-            productPojo.setSourcePrice(Double.parseDouble(product[2]));
-            productPojo.setRetailPrice(Double.parseDouble(product[3]));
-        }
+    public void printProducts() {
+        String productString = "";
+        for (int i = 0; i < csvToList().size(); i++) {
+        productString = Arrays.toString(csvToList().get(i));
+            System.out.println(productString);
+            }
     }
-
-
 
     //search products by keyword and list output method
     public void keywordSearch(String keyword) {
@@ -55,35 +51,79 @@ public class Processor {
         }
     }
 
-    //create new productPojo with input for attributes
-    public ProductPojo newProductPojoFromInput (String productUpc, String productName, double sourcePrice, double retailPrice) {
-        ProductPojo newProductPojo = new ProductPojo();
-        newProductPojo.setProductUpc(productUpc);
-        newProductPojo.setProductName(productName);
-        newProductPojo.setSourcePrice(sourcePrice);
-        newProductPojo.setRetailPrice(retailPrice);
-        return newProductPojo;
+    //create new product String with input for attributes
+    public String newProductFromInput () {
+        String newProduct;
+        System.out.print("UPC: ");
+        String upcInput = userInput.nextLine();
+        System.out.print("Product Name: ");
+        String prodNameInput = userInput.nextLine();
+        System.out.print("Source Price: ");
+        double sourcePriceInput = userInput.nextDouble();
+        System.out.print("Retail Price: ");
+        double retailPriceInput = userInput.nextDouble();
+        newProduct = upcInput + ", " + prodNameInput + ", " + Double.toString(sourcePriceInput) + ", " + Double.toString(retailPriceInput);
+        return newProduct;
     }
 
-    //input new item, including printwriter method , still needs a bunch of work! Not actually writing to the file.
-    public void newInputProductPojo (ProductPojo productPojo) {
-        String[] newInputArray = new String[]{productPojo.getProductUpc(), productPojo.getProductName(), Double.toString(productPojo.getSourcePrice()), Double.toString(productPojo.getRetailPrice())};
-        try {
-//            FileWriter fileWriter = new FileWriter("custom_file.txt", true);
-            PrintWriter printWriter = new PrintWriter("custom_file.txt");
-            printWriter.println(Arrays.toString(newInputArray));
-        } catch (FileNotFoundException e) {
+    //filewrites newProduct to designate file
+    public void newInputProduct () {
+        String destination = "custom_file.csv";
+
+        try (FileWriter fileWriter = new FileWriter(destination, true)) {
+            fileWriter.write(newProductFromInput());
+            System.out.println("Thank you for adding a product to " + destination);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
 
     }
-    //ascertain recommendations based on desired profit margin and printwrite to recommendations txt file
 
+public boolean within5Percent(double x, double y) {
+        double highX = x + (x * .05);
+        double lowX = x - (x * .05);
+        if (y <= highX && y >= lowX) {
+            return true;
+        }
+        return false;
+    }
+
+
+    //ascertain recommendations based on desired profit margin and printwrite to recommendations txt file
+public List<String> getRecommendations() {
+        System.out.print("Please input a whole percentage for your desired profit margin: ");
+        String profitMargin = userInput.nextLine();
+        int percentage = Integer.parseInt(profitMargin);
+        for (String[] product : csvToList()) {
+            String recommendationFull = "";
+            double marginPrice = Double.parseDouble(product[2]) * (1 + percentage / 100.0);
+            if (within5Percent(Double.parseDouble(product[3]), marginPrice)) {
+                recommendationFull = product[0] + " Margin price: " + marginPrice + ". " + listWithCaution;
+            } else if (marginPrice > Double.parseDouble(product[3])) {
+                recommendationFull = product[0] + " Margin price: " + marginPrice + ". " + listFalse;
+            } else if (marginPrice < Double.parseDouble(product[3])) {
+                recommendationFull = product[0] + " Margin price: " + marginPrice + ". " + listAffirmative;
+            }
+            recommendations.add(recommendationFull);
+        }
+        return recommendations;
+}
 
     //create method to printwrite recommendations to txt file
+public void printRecs() {
+        getRecommendations();
+        String destinationFile = "Recommendations_for_" + LocalDate.now();
 
+        try (PrintWriter printWriter = new PrintWriter(destinationFile)) {
+            String recommendationsPrint = "";
+            for (int i = 0; i < recommendations.size(); i++) {
+                recommendationsPrint = recommendations.get(i);
+                printWriter.println(recommendationsPrint);
+            }
+            System.out.println("Thanks. Please see your recommendations in " + destinationFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+}
 
 }
