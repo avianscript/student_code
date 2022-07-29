dfba66ca25ee5bff74c443f841b1456111351889
Yoav Morahg <yoav@techelevator.com>
2022-07-29 08:14:16 -0400

    m3d14 kecture

---
+++ module-3/14_Review_Day/review-student/src/components/ProductImage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="img-div">
+    <!-- src property should be set based on image being displayed -->
+    <img src="" />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'product-image'
+    /* this component should receive the url to use via an imgUrl attribute*/
+}
+</script>
+
+<style scoped>
+    .img-div {
+        width: 200px;
+        height: 200px;
+        border-right: 2px solid black;
+        padding: 5px;
+    }
+
+    img {
+        max-width: 100%;
+        max-height: 100%;
+    }
+</style>
---
+++ module-3/14_Review_Day/review-student/src/components/ProductsPanel.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+        <!-- this should display all products -->
+        <div class="product-container" id="product-row" >
+            <!-- add the ProductImage component, set its id to img-panel, and pass the product's product_image_url to it -->
+            
+            <!-- add the ProductDetail component, set is id to text-panel, and pass the current product to it --> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'products-panel',
+    components: {
+    },
+    computed: {
+        /* create a property called filteredProducts to filter the products by search 
+            once search is implemented. make sure it actually
+            filters the products on the page */
+    }
+
+}
+</script>
+
+<style>
+    .product-container {
+        display: grid;
+        grid-template-columns: 1fr 9fr;
+        grid-template-areas: "img-panel text-panel";
+        border: 2px solid black;
+        margin-right: 20px;
+
+    }
+
+    #img-panel {
+        grid-area: img-panel;
+    }
+
+    #text-panel {
+        grid-area: text-panel;
+        background-color: #f2f2f2;
+        padding-left: 20px;
+    }
+
+    .attribution {
+        font-size: 10px;
+    }
+
+    .cat-filter {
+        width: 50%;
+        padding-bottom: 10px;
+    }
+</style>
